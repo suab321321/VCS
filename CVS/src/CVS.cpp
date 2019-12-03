f8349fe7ad2429987cc0146aaeb6ce052801eb6a
AbhinavSingh <singhabhinav9051571833@gmail.com>
2019-12-03 18:41:13 +0530

    Blob Ready

---
+++ CVS/src/CVS.cpp
@@ -0,0 +1,36 @@
+//#include<boost/archive/binary_iarchive.hpp>
+//#include<boost/archive/binary_oarchive.hpp>
+
+#include<fstream>
+#include <iostream>
+#include "../headers/Blob.hpp"
+#include "../headers/Tree.hpp"
+#include "../headers/Commit.hpp"
+#include <boost/filesystem.hpp>
+#include<fstream>
+#include<unordered_set>
+#include<unistd.h>
+
+namespace fs=boost::filesystem;
+using namespace std;
+
+int main() {
+//	const string path="/home/abhinav/cpp-2019-09/prgrames/CVS";
+	boost::filesystem::path full_path(boost::filesystem::current_path());
+//	cout<<full_path<<endl;
+//	for(fs::directory_entry& e:fs::recursive_directory_iterator(full_path))
+//		cout<<e.path().filename()<<endl;
+	cout<<"git add------->git commit------->git push"<<endl;
+	int choice;
+	cin>>choice;
+	switch(choice){
+		case 1:add(full_path.string());
+			break;
+//		case 2:commit();
+//			break;
+//		case 3:push();
+//			break;
+		default:
+			cout<<"wrong choice"<<endl;
+	}
+}
